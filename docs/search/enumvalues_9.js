52bc0d94b47e6391be3066aa92ad5abf40d664b7
NhanLT <trongnhan.uit@gmail.com>
2023-08-21 17:09:44 +1000

    Update docs

--- docs/search/enumvalues_9.js
+++ docs/search/enumvalues_9.js
@@ -1,14 +1,12 @@
 var searchData=
 [
-  ['model_5fauto_0',['MODEL_AUTO',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1a5bbe0ea9b788df22aefa117b7c7c13ae',1,'cmaple::ModelBase']]],
-  ['model_5funknown_1',['MODEL_UNKNOWN',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1a4743d942928b86150b442adffa58520c',1,'cmaple::ModelBase']]],
-  ['more_5faccurate_5ftree_5fsearch_2',['MORE_ACCURATE_TREE_SEARCH',['../classcmaple_1_1_tree.html#a39bd3bfce400c9222e8b77c584d26d43ae452d25b3b1a6060c8f156d0a35566d2',1,'cmaple::Tree']]],
-  ['mtart_3',['MTART',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1aa5469cdc567636bf3ab8da9f9e814671',1,'cmaple::ModelBase']]],
-  ['mtinv_4',['MTINV',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1a110713d6b4ca73c6a966894cef3bc1ce',1,'cmaple::ModelBase']]],
-  ['mtmam_5',['MTMAM',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1a8ed5942d04cf10d0ae71eb9479a77a3c',1,'cmaple::ModelBase']]],
-  ['mtmet_6',['MTMET',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1aa04718aa8a73218a3a7036aae3f400c7',1,'cmaple::ModelBase']]],
-  ['mtrev_7',['MTREV',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1abf1d1939504b025f25810fa4215fc21e',1,'cmaple::ModelBase']]],
-  ['mtver_8',['MTVER',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1a27e9c72ff91e535d8589c4817c7978ce',1,'cmaple::ModelBase']]],
-  ['mtzoa_9',['MTZOA',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1af5376f8b46c30fd5ff3193548ded18e9',1,'cmaple::ModelBase']]],
-  ['mul_5ftree_10',['MUL_TREE',['../classcmaple_1_1_tree.html#a750c5e7c53559fbcc421020bad665376a0156cc734ae126b57d1a6ceeb22bd9e9',1,'cmaple::Tree']]]
+  ['more_5faccurate_5ftree_5fsearch_0',['MORE_ACCURATE_TREE_SEARCH',['../classcmaple_1_1_tree.html#a39bd3bfce400c9222e8b77c584d26d43ae452d25b3b1a6060c8f156d0a35566d2',1,'cmaple::Tree']]],
+  ['mtart_1',['MTART',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1aa5469cdc567636bf3ab8da9f9e814671',1,'cmaple::ModelBase']]],
+  ['mtinv_2',['MTINV',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1a110713d6b4ca73c6a966894cef3bc1ce',1,'cmaple::ModelBase']]],
+  ['mtmam_3',['MTMAM',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1a8ed5942d04cf10d0ae71eb9479a77a3c',1,'cmaple::ModelBase']]],
+  ['mtmet_4',['MTMET',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1aa04718aa8a73218a3a7036aae3f400c7',1,'cmaple::ModelBase']]],
+  ['mtrev_5',['MTREV',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1abf1d1939504b025f25810fa4215fc21e',1,'cmaple::ModelBase']]],
+  ['mtver_6',['MTVER',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1a27e9c72ff91e535d8589c4817c7978ce',1,'cmaple::ModelBase']]],
+  ['mtzoa_7',['MTZOA',['../classcmaple_1_1_model_base.html#aad9cb5622678fdf128b3ea016b475ee1af5376f8b46c30fd5ff3193548ded18e9',1,'cmaple::ModelBase']]],
+  ['mul_5ftree_8',['MUL_TREE',['../classcmaple_1_1_tree.html#a750c5e7c53559fbcc421020bad665376a0156cc734ae126b57d1a6ceeb22bd9e9',1,'cmaple::Tree']]]
 ];
